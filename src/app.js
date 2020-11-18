3d8b89a1460bed49348cc869376b297e7efb12d7
ashen-develops <ashenp.designs@gmail.com>
2020-11-18 10:57:06 -0600

    changed app, config, and .env to be ready for routers

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config')
+const { NODE_ENV } = require('./config');
+const notesRouter = require('./notes/notes-router');
+const foldersRouter = require('./folders/folders-router');
 
 const app = express();
 
@@ -15,21 +17,25 @@ app.use(morgan(morganOption));
 
 app.use(helmet());
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+app.use(cors());
+
+app.use('/notes', notesRouter);
+app.use('/folders', foldersRouter);
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, world!');
+// });
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === 'production') {
     response = { error: { message: 'server error' } };
   } else {
+    // eslint-disable-next-line no-console
     console.error(error);
     response = { message: error.message, error };
   }
   res.status(500).json(response);
 });
-
-app.use(cors());
 
 module.exports = app;
